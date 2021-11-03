bdf9f7609031c345d37959cc20d8b8ecea0736bf
fpineirolopez <pineiro.felipe@gmail.com>
2021-11-03 12:44:52 -0400

    feat(UI): connect api to react app, tested payments endpoint on button click

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 import '../styles/App.css';
-import Button from 'react-bootstrap/Button';
+import {Button} from 'react-bootstrap';
 
 function App() {
+
+  // const [chores, expenses] = React.useState("");
+
+  function testBtn(text) {
+    alert('Clicked ' + text + ' button');
+  }
+  
+  async function fetchExpenses(){
+    fetch("/expenses")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  } 
+
   return (
     <div className="App">
-      <Button variant="primary">Chores</Button>
-      <Button variant="dark">Expenses</Button>
+      <Button className="chore-btn" variant="primary" onClick={() => testBtn('Chores')}><h1>Chores</h1></Button>
+      <Button className="expense-btn" variant="dark" onClick={() => fetchExpenses()}><h1>Expenses</h1></Button>
     </div>
   );
 }
